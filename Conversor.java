5adb5d14cffaaa47cf6bd53d551559a20608716a
Emiliano de la Torre H <emiliano.delatorre9058@alumnos.udg.mx>
2023-09-11 16:03:32 -0600

    Main documentado

--- Conversor.java
+++ Conversor.java
@@ -15,14 +15,14 @@ public class Conversor {
     /**
      * el Frontend donde el usuario introduce lo que desea y se muestra lo 
      * que quiere*/
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner leer = new Scanner(System.in); //es la forma de introducir datos
         Librerias formato = new Librerias(); //Objeto de librerias
         String numero = "";
         int opcion1 =0, opcion2 = 0;//varible usada para cerrar el ciclo y variable con la que se trabajara
-        while (opcion1!=5 || opcion2!=5)
+        while (opcion1!=5 || opcion2!=5) //While para que el usuario decida cuando romperlo//
         {   
-            System.out.println("Escribe el numero que quieres convertir: ");
+            System.out.println("Escribe el numero que quieres convertir: "); //Numero a convertir
             numero = leer.next();
             System.out.println("|--------------------------------------|");
             System.out.println("|1) Binario                            |");
@@ -37,7 +37,7 @@ public class Conversor {
             opcion2 = leer.nextInt();
             if (opcion1 != opcion2) //If para verificar que ambas opciones no sean iguales
                 numero = formato.convertidor(opcion1, opcion2, numero); //Backend
-            if (numero == "") { //Comienzo del ifelse
+            if (numero == "") { //Comienzo del ifelse, le avisa al usuario que no escribio nada
                 System.out.println("No Te VaLlAs a CanSaR DE EScRiBir >:(");
                 System.out.println("Traduccion: Formato de numero invalido");
             } else {
