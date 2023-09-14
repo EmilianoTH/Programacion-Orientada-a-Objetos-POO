06c3c305ef68689a82d83e037f786d1b7c4fe666
Emiliano de la Torre H <emiliano.delatorre9058@alumnos.udg.mx>
2023-09-14 14:45:58 -0600

    Libreria personal

---
+++ LibreriapersonaldeEmiliano.java
@@ -0,0 +1,11 @@
+public class LibreriapersonaldeEmiliano {
+    public static void MaindeUnidades(){
+        System.out.println("\n\n|--------------------------------------|");
+        System.out.println("|1) Binario                            |");
+        System.out.println("|2) Octal                              |");
+        System.out.println("|3) Decimal                            |");
+        System.out.println("|4) Hexadecimal                        |");
+        System.out.println("|5) Salir                              |");
+        System.out.println("|--------------------------------------|");
+    }
+}
