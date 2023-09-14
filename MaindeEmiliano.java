d5629cd3042636781a1a9d0669e97bfa932a0056
Emiliano de la Torre H <emiliano.delatorre9058@alumnos.udg.mx>
2023-09-14 15:08:10 -0600

    Continuaciond e main

--- MaindeEmiliano.java
+++ MaindeEmiliano.java
@@ -18,8 +18,15 @@ public class MaindeEmiliano {
      * que quiere*/
     public static void main(String[] args) { //Comienzo del main
         Scanner leer = new Scanner(System.in); //es la forma de introducir datos
-        int menu = 0, opcion1 = 0, opcion2 = 0; //Variables a utilizar
+        Librerias formato = new Librerias(); //Objeto de librerias
+        int menu = 0; //Variables a utilizar
+        double unidad = 0; //Variable que utilizara las unidades
+        float temperatura = 0; //Variable que utilizara la temperatura
+        String sistemas = ""; //Variable que utilizara los sistemas
+
         while (menu != 4) {
+        int  opcion1 = 0, opcion2 = 0;
+
             System.out.println("|--------------------------------------|");
             System.out.println("|1) Sistemas                           |");
             System.out.println("|2) Unidades                           |");
@@ -32,6 +39,42 @@ public class MaindeEmiliano {
             } catch (Exception e) {
                 leer.nextLine();
                 System.out.println("\nError\nElige una opcion correcta");
+            }
+            switch (menu) {
+                case 1:
+                    System.out.println("Escribe el numero que quieres convertir: "); //Numero a convertir
+                    sistemas = leer.next();
+                    LibreriapersonaldeEmiliano.MaindeUnidades();
+                    try {
+                        System.out.println("Escoge el formato en el que escribiste el numero: ");
+                        opcion1 = leer.nextInt();
+                        System.out.println("Escoge el formato en el que quieres convertir el numero que has escribido: ");
+                        opcion2 = leer.nextInt();
+                    } catch (Exception e) {
+                        System.out.println("\nError\nElige una opcion correcta");
+                    }
+                    if (opcion1 == 5 || opcion2 == 5) //if
+                        System.out.println("Entendido");
+                    else{ //Else
+                        if (opcion1 != opcion2) //If para verificar que ambas opciones no sean iguales
+                        sistemas = formato.convertidor(opcion1, opcion2, sistemas); //Backend
+                        if (sistemas == "") { //Comienzo del ifelse, le avisa al usuario que no escribio nada
+                        System.out.println("\n\nNo Te VaLlAs a CanSaR DE EScRiBir >:(");
+                        System.out.println("Traduccion: Formato de numero invalido");
+                        } else {
+                        System.out.println("Resultado: "+sistemas);
+                        } //Fin del ifelse
+                    }//Fin del else
+                    break;
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+                default:
+                    break;
             }
         } //Fin del while
     leer.close(); //Cerrar el scaner
