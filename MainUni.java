c9d0697dafc6fb572753d920b3b8fd85da0d6c98
KillMidory <123518287+KillMidory@users.noreply.github.com>
2023-09-15 11:34:38 -0600

    MainUni.java
    El main de Midory de las unidades sin Static

--- MainUni.java
+++ MainUni.java
@@ -17,8 +17,10 @@ public class MainUni {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in); //es la forma de introducir datos
         LibreriasdeUnidades formato = new LibreriasdeUnidades(); //Objeto de librerias
-        double numero = 0;
-        int opcion1 =0, opcion2 = 0;//varible usada para cerrar el ciclo y variable con la que se trabajara
+        double numero = 0;//debe ser doble para que tenga un mayor espacio de numeros y entre los flotantes
+        int opcion1 =0, opcion2 = 0;//variables para seleccionar formato de entrada y salida
+        //este while se encarga de mantener un ciclo en el programa hasta que el usuario decida cerrarlo 
+        //seleccionando ambas opciones el numero 7
         while (opcion1!=7 || opcion2!=7)
         {   
             System.out.println("Escribe el numero que quieres convertir: ");
@@ -36,13 +38,14 @@ public class MainUni {
             opcion1 = leer.nextInt();
             System.out.println("Escoge el formato en el que quieres convertir el numero que has escribido: ");
             opcion2 = leer.nextInt();
-            if (opcion1 != opcion2) //if para verificar que ambas opciones no sean iguales
+            //if para verificar que ambas opciones no sean iguales
+            if (opcion1 != opcion2) 
                 formato.mostrar(numero, opcion1, opcion2);//Backend
             if (numero == 0) { //Comienzo del ifelse;
                 System.out.println("Formato de numero invalido");
             } //fin del if
+            else { } //Fin del ifelse
         }//fin del while
         leer.close();
     }//fin del main
-
 }//fin de la clase principal 
