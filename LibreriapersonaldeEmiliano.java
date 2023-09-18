3b360a50bf64a055dd37db03c1a466fee2e17a10
Emiliano de la Torre H <emiliano.delatorre9058@alumnos.udg.mx>
2023-09-18 10:45:19 -0600

    Main necesarios

--- LibreriapersonaldeEmiliano.java
+++ LibreriapersonaldeEmiliano.java
@@ -1,11 +1,32 @@
 public class LibreriapersonaldeEmiliano {
-    public static void MaindeUnidades(){
-        System.out.println("\n\n|--------------------------------------|");
+    public static void MaindeSistemas(){
+        System.out.println("\n\n|----------------------------------|");
         System.out.println("|1) Binario                            |");
         System.out.println("|2) Octal                              |");
         System.out.println("|3) Decimal                            |");
         System.out.println("|4) Hexadecimal                        |");
         System.out.println("|5) Salir                              |");
         System.out.println("|--------------------------------------|");
+    }
+
+    public static void MaindeUnidades(){
+        System.out.println("\n\n|----------------------------------|");
+        System.out.println("|1) Centimetros                        |");
+        System.out.println("|2) Pulgadas                           |");
+        System.out.println("|3) Yardas                             |");
+        System.out.println("|4) Metro                              |");
+        System.out.println("|5) Kilometros                         |");
+        System.out.println("|6) Millas                             |");
+        System.out.println("|7) Salir                              |");
+        System.out.println("|--------------------------------------|");
+    }
+
+    public static void MainTemperaturas(){
+        System.out.println("\n\n|--------------------------------------|");
+        System.out.println("|1) Kelvin                                 |");
+        System.out.println("|2) Fahrenheit                             |");
+        System.out.println("|3) Celsius                                |");
+        System.out.println("|4) Salir                                  |");
+        System.out.println("\n\n|--------------------------------------|");
     }
 }
