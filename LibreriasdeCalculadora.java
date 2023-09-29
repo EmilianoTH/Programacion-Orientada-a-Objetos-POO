01a98667d9839f32892c83719bfc4556f283b154
Emiliano de la Torre H <emiliano.delatorre9058@alumnos.udg.mx>
2023-09-28 21:30:15 -0600

    Documentacion de javadoc

--- LibreriasdeCalculadora.java
+++ LibreriasdeCalculadora.java
@@ -10,24 +10,65 @@
  */
 
 public class LibreriasdeCalculadora {
-    
+    /* Metodos */
+
+    /**
+     * 
+     * @param num1 Numero a sumar
+     * @param num2 Numero a sumar
+     * @return Retornar la suma de ambos numeros
+     */
     private float suma(float num1, float num2){
         return num1+num2;
     }
 
+    /**
+     * 
+     * @param num1 Numero a restar
+     * @param num2 Numero a restar
+     * @return Retornar la resta de ambos numeros
+     */
     private float resta(float num1, float num2){
         return num1-num2;
     }
-
+    
+    /**
+     * 
+     * @param num1 Numero a multiplicar
+     * @param num2 Numero a multiplicar
+     * @return Retornar la multiplicacion de ambos numeros
+     */
     private float multi(float num1, float num2){
         return num1*num2;
     }
 
+    /**
+     * 
+     * @param num1 Dividendo
+     * @param num2 Divisor
+     * @return Retornar la division de ambos numeros
+     */
     private float division(float num1, float num2){
         return num1/num2;
     }
 
+    /**
+     * 
+     * @param num Numero a sacar la raiz cuadrada
+     * @return Retornar raiz cuadrada
+     */
     private double raiz(double num){
         return Math.sqrt(num);
+    }
+    
+    /**
+     * 
+     * @param num1 Numero a sumar
+     * @param num2 Numero a sumar
+     */
+    public void mostrarsuma(float num1, float num2){
+        float resultado = 0;
+        resultado = suma(num1, num2);
+        System.out.println("El resultado de tu suma es: "+resultado);
     }
 }
