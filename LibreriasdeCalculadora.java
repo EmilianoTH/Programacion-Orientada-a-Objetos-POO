b928acb16aad65e1584390a877e1f29e2a866296
Emiliano de la Torre H <emiliano.delatorre9058@alumnos.udg.mx>
2023-09-28 21:16:25 -0600

    Operaciones basicas

---
+++ LibreriasdeCalculadora.java
@@ -0,0 +1,36 @@
+/* Emiliano De La Torre Hernandez
+ * Mariana Midory Iniguez Rodriguez
+ * Angel Eduardo Munoz Perez 
+ * 28-09-2023
+ * Librerias de temperaturas
+ * Centro Universitario de los Altos | Universidad de Guadalajara
+ * Ingenieria en computacion
+ * Profesor: Sergio Franco Casillas
+ * Descripcion: convierte el ingreso del valor a la temperatura que tu hayas elegido
+ */
+
+public class LibreriasdeCalculadora {
+    
+    private float suma(float num1, float num2){
+        float resul = 0;
+
+        return num1+num2;
+    }
+
+    private float resta(float num1, float num2){
+        float resul = 0;
+
+        return num1-num2;
+    }
+
+    private float multi(float num1, float num2){
+        float resul = 0;
+
+        return num1*num2;
+    }
+
+    private float division(float num1, float num2){
+        float resul = 0;
+        return num1/num2;
+    }
+}
