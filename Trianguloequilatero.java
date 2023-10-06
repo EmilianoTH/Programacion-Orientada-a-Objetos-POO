cbabd384877f4cbdeaa2483eda6645cfa3d3c441
Emiliano de la Torre H <emiliano.delatorre9058@alumnos.udg.mx>
2023-10-05 22:34:04 -0600

    Clase finalizada

--- Trianguloequilatero.java
+++ Trianguloequilatero.java
@@ -1,19 +1,42 @@
+package fg;
 
 /**
+ *
  * @author Emiliano
  */
-public class Trianguloequilatero extends Figurasgeometricas{
-    public Trianguloequilatero(){
-        super.setSide1();
+public class TrianguloEquilatero extends Figurasgeometricas{
+    private float altura = 0; //Atributo necesario
+    
+    public TrianguloEquilatero(){
+        super.setSide1(0); //Atributo de base
+        this.altura = 0; //Atributo de altura
     }
-
+    
+    /**
+     * 
+     * @param x Atributo que pondra el usuario de la base
+     * @param y Atributo que pondra el usuario de la altura
+     */
+    public TrianguloEquilatero(float x, float y){
+        super.setSide1(x); 
+        this.altura = y; 
+    }
+    
+    /**
+     * 
+     * @return Reetorna el perimetro de un triangulo equilatero
+     */
     @Override
     public float getPerimeter() {
-        return (float) (Math.PI * (super.getSide1()*2));
+        return super.getside1()*3;
     }//fin del clase 
-
+    
+    /**
+     * 
+     * @return retorna el area de un triangulo equilatero
+     */
     @Override
     public float getArea() {
-        return (float) (Math.PI * (super.getSide1()* (super.getSide1())));
+        return (super.getside1()*altura)/2;
     }//fin de la clase de area
-} //Fin de clase
+} //Fin de la clase
