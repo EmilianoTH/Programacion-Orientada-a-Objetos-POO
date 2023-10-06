b03559fd56a084501edbcaf60688b11f8a27c995
Emiliano de la Torre H <emiliano.delatorre9058@alumnos.udg.mx>
2023-10-05 22:10:11 -0600

    Override

--- Trianguloequilatero.java
+++ Trianguloequilatero.java
@@ -6,4 +6,14 @@ public class Trianguloequilatero extends Figurasgeometricas{
     public Trianguloequilatero(){
         super.setSide1();
     }
+
+    @Override
+    public float getPerimeter() {
+        return (float) (Math.PI * (super.getSide1()*2));
+    }//fin del clase 
+
+    @Override
+    public float getArea() {
+        return (float) (Math.PI * (super.getSide1()* (super.getSide1())));
+    }//fin de la clase de area
 } //Fin de clase
