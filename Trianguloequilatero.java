c55d8910d15f5b4952e520d3d794efb793b6686f
Emiliano de la Torre H <emiliano.delatorre9058@alumnos.udg.mx>
2023-10-07 20:00:37 -0600

    Triangulo equilatero

--- Trianguloequilatero.java
+++ Trianguloequilatero.java
@@ -5,8 +5,9 @@ package fg;
  * @author Emiliano
  */
 public class TrianguloEquilatero extends Figurasgeometricas{
-    private float altura = 0; //Atributo necesario
+    private float altura; //Atributo necesario
     
+    /* Constructores */
     public TrianguloEquilatero(){
         super.setSide1(0); //Atributo de base
         this.altura = 0; //Atributo de altura
@@ -21,6 +22,21 @@ public class TrianguloEquilatero extends Figurasgeometricas{
         super.setSide1(x); 
         this.altura = y; 
     }
+    
+    /* Set y get */
+    /**
+     * 
+     * @param x Altura del triangulo
+     */
+    public void setaltura(float x){
+        this.altura = x;
+    }
+    //Conseguir la altura
+    public float getaltura(){
+        return this.altura;
+    }
+    
+    /* Metodos */
     
     /**
      * 
@@ -37,6 +53,6 @@ public class TrianguloEquilatero extends Figurasgeometricas{
      */
     @Override
     public float getArea() {
-        return (super.getside1()*altura)/2;
+        return (super.getside1()*getaltura())/2;
     }//fin de la clase de area
 } //Fin de la clase
